d63cc8194d973819d0e6bbb0efc0777a921c1e23
Joshua Prendergast <j.prendo10@gmail.com>
2013-11-20 15:10:21 +0000

    Fix task 3, work on task 4

--- src/main.asm
+++ src/main.asm
@@ -7,22 +7,23 @@ __config _WDT_OFF & _RC_OSC & _PWRTE_OFF	; Watchdog timer off, RC oscillator, po
 
 cblock	0x20			; Registers
 	pattern				; Argument used by BITMASK to set lights
+	acc1				; General counter
+	acc2				; General counter
 endc
 
 INIT:
 	BSF 	STATUS,	RP0 ; Set register bank select bit; use Bank1
 	
-	
 	CLRF	TRISB		; Clear TRISB; PORTB becomes output
 						
 						; Setup PORTA
-	MOVLW	0x01		; Load 1 into W
+	MOVLW	0xFF		; Load 255 into W
 	MOVWF	TRISA		; Set TRISA; PORTA becomes input
 	
 	BCF		STATUS, RP0	; Use Bank0 again
 	CLRF	PORTA		; Clear PORTA
 
-	GOTO	SWITCHES	; Goto main body
+	GOTO	COUNT		; Goto main body
 
 ; Task 1 - Activates all the lights.
 ALL:
@@ -41,9 +42,39 @@ BITMASK:
 ; Task 3 - Activates lights whose buttons are pressed. Only the first 4 lights are affected.
 SWITCHES:
 	MOVF	PORTA, 0	; Read input bitmask into W
-	ANDLW	0x15		; Keep the bottom 4 bits
+	XORLW	0x0F		; Flip input and keep the bottom 4 bits
 	MOVWF	PORTB		; Move W into PORTB
 
 	GOTO SWITCHES
 
+; Task 4 - Activates X lights where X is the number of pressed buttons.
+; FIXME
+COUNT:
+	MOVLW	0x05
+	MOVWF	acc1		; acc1 = 5
+
+	MOVF	PORTA, 0	; Read input into W
+	XORLW	0x0F		; Flip input and keep the bottom 4 bits
+
+COUNT_COUNT:
+	RRF		W, 1		; Rotate one bit out, store in W
+	BTFSC	STATUS, C	; If the carried out bit was 0 (off), skip incrementing
+	DECF	acc1, 1		; acc1--
+
+	IORLW	0x00		; 
+	BTFSC	STATUS, Z	; Check if W is zero
+	GOTO	COUNT_COUNT	; Not zero, continue counting
+	
+						; acc1 contains number of active lights
+	MOVLW	0x0F		; All lights on
+	
+COUNT_MASK:
+	DECFSZ	acc1		; Loop acc1 - 1 times
+						; Build output mask		
+	RRF		W, 1		; Shift W right
+	GOTO	COUNT_MASK
+
+	MOVWF	PORTA
+
+	GOTO 	COUNT
 	END					; Listing end
