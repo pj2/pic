1a44cd938ca822607bee7704b8d80daa82429ea3
Joshua Prendergast <j.prendo10@gmail.com>
2013-11-27 00:43:29 +0000

    Add task 6, move delay into subroutine

--- src/main.asm
+++ src/main.asm
@@ -11,6 +11,8 @@ cblock	0x20			; Registers
 	acc2				; General counter
 	acc3				; General counter
 	acc4				; General counter
+	p1					; Parameter 1
+	p2					; Parameter 2
 endc
 
 ; Program
@@ -35,7 +37,7 @@ INIT:
 	MOVLW	0x0D		; pattern = 13 (if 0 is on and - is off, looks like this: 0-0-0--)
 	MOVWF	pattern	
 
-	GOTO	BINARY		; Goto main body
+	GOTO	KNIGHT		; Goto main body
 
 ; Task 1 - Activates all the lights.
 ALL:
@@ -84,16 +86,11 @@ COUNT:
 
 ; Task 5 - Counts from 0 to 255 every 30 seconds.
 BINARY:
-	MOVLW	0xA7		; acc2 = 167
-	MOVWF	acc2
-	MOVLW	0x35		; acc3 = 53
-	MOVWF	acc3
-delay_0:				; This loop executes around 255(acc3 - 1) + acc2 - 1 times
-	DECFSZ	acc2, F		; It contains 6 cycles worth of instructions per loop, therefore
-	GOTO	$+2			; it executes ~80556 instructions. That takes about 0.117s
-	DECFSZ	acc3, F		; meaning the entire BINARY loop takes roughly 30 seconds (0.117 * 256) to complete.
-	GOTO 	delay_0		
-						; Adapted from code generated here: http://www.piclist.com/techref/piclist/codegen/delay.htm
+	MOVLW	0xA7		; p1 = 167
+	MOVWF	p1
+	MOVLW	0x35		; p2 = 53
+	MOVWF	p2
+	CALL	delay
 
 	MOVF	acc1, W		; W = acc1
 	MOVWF	PORTB		; PORTB = W
@@ -105,11 +102,59 @@ delay_0:				; This loop executes around 255(acc3 - 1) + acc2 - 1 times
 	BTFSC	STATUS, Z	; Check if acc1 = 255
 	CLRF	acc1		; If acc1 ^ 255 = 0 (AKA acc1 = 255), set acc1 to 0
 	
-	GOTO BINARY
+	GOTO 	BINARY
 
+; Task 6 - Knight rider.
+KNIGHT:
+	MOVLW	0x01		; Initial setup - only runs once
+	MOVWF	acc1		; acc1 = 1 (light state)
+loop_0:
+	MOVF	acc1, W		; W = acc1
+	MOVWF	PORTB		; PORTB = acc1
+	
+	MOVLW	0x3A		; p1 = 58
+	MOVWF	p1
+	MOVLW	0x4C		; p2 = 76
+	MOVWF	p2
+	CALL	delay		; About 0.1333s per loop (15Hz)
 
+	BTFSS	acc2, 0x00
+	GOTO	test_right	; If acc2 = 0, try to move right (board direction, not binary direction)
+	GOTO	test_left	; Otherwise, try to move left
 
+test_right:
+	INCF	acc1, W		; If acc1 == 128, W will be >128
+	GOTO	end_0
 
+test_left:
+	MOVLW	0x02
+	SUBWF   acc1, W		; If acc1 == 1, W will overflow and therefore be >128
+	GOTO	end_0
+
+end_0:
+	SUBLW	0x80
+	BTFSS	STATUS, C	; If the shifted value is > 128, flip direction
+	COMF	acc2, F		; This happens if 1 is shifted right (overflows to 255) or 128 is shifted left
+
+	BCF		STATUS, C	; Clear C so we shift in zeros
+	BTFSS	acc2, 0x00
+	GOTO	right		; acc2 = 0, move right
+	GOTO	left		; acc2 = 0xFF, move left
+right:
+	RLF		acc1, F
+	GOTO 	loop_0
+left:
+	RRF		acc1, F
+	GOTO	loop_0
+
+; Functions
+delay:
+	DECFSZ	p1, F		; This loop executes around 255(p2 - 1) + p1 - 1 times
+						; There are 6 cycles worth of instructions per loop, therefore
+	GOTO	$+2			; if p1 = 167 and p2 = 53 we execute ~80556 instructions. That takes about 0.117s
+	DECFSZ	p2, F		; meaning the entire BINARY loop takes roughly 30 seconds (0.117 * 256) to complete.
+	GOTO 	delay		
+	RETURN				; Adapted from code generated here: http://www.piclist.com/techref/piclist/codegen/delay.htm
 
 	END					; Listing end
 
